44378f712b69106c908677d9d2dbf36b13909c94
suma <suma.gmail.com>
2025-03-24 23:07:10 +0530

    managing Dependancy

---
+++ Community.java
@@ -0,0 +1,8 @@
+class Community{
+    public static void povideTicket(){
+        System.out.println("inside provide tickets");
+        Participants.buyTickets();
+        System.out.println("provide tickets ended");
+    }
+    
+}
---
+++ Participants.java
@@ -0,0 +1,11 @@
+class Participants{
+    public static void buyTickets(){
+        System.out.println("inside buy tickets");
+        entertain();
+        System.out.println("buyTickets ended");
+    }
+    public static void entertain(){
+        System.out.println("entertain executed");
+    }
+    
+}
